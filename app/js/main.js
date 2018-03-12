c6be5394544dc05efc1d6938fc368924198e9c47
Mikołaj Jarmuła <mikolaj_jarmula@wp.pl>
2018-03-12 15:14:52 +0100

    Present navbar

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Hello');
+
+    // ### HAMBURGER ###
+    const hamburger = document.querySelector('.hamClick');
+    const navMobile = document.querySelector('.navMobile');
+
+    hamburger.addEventListener('click', function () {
+        this.classList.toggle('active');
+        navMobile.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function () {
+        if (document.activeElement !== hamburger) {
+            hamburger.classList.remove('active');
+            navMobile.classList.remove('active');
+        }
+    });
+    // ### END ###
+
 });
