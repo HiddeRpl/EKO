d2c629975004b9f8df08fb16c244cfd3e499a54f
Mikołaj Jarmuła <mikolaj_jarmula@wp.pl>
2018-03-13 16:53:12 +0100

    Add slider

--- app/js/main.js
+++ app/js/main.js
@@ -17,4 +17,45 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     // ### END ###
 
+    // ### SLIDER ###
+    const sliderWrap = document.querySelector('.slider-wrapper');
+    const sliderLeft = document.querySelector('.goLeft');
+    const sliderRight = document.querySelector('.goRight');
+    let slider = 0;
+
+    function moveSlider(direction) {
+        clearInterval(sliderMoving);
+        sliderWrap.classList.remove(`slider-${slider}`);
+        if (direction === 'left') {
+            slider--;
+            if (slider < 0) {
+                slider = 2;
+            }
+        }
+        else {
+            slider++;
+            if (slider > 2) {
+                slider = 0;
+            }
+        }
+        sliderWrap.classList.add(`slider-${slider}`);
+    }
+
+    const sliderMoving = setInterval(() => {
+        sliderWrap.classList.remove(`slider-${slider}`);
+        slider++;
+        if (slider > 2) {
+            slider = 0;
+        }
+        sliderWrap.classList.add(`slider-${slider}`);
+    }, 5000);
+
+    sliderLeft.addEventListener('click', () => {
+        moveSlider('left');
+    });
+    sliderRight.addEventListener('click', () => {
+        moveSlider('right');
+    });
+    // ### END ###
+
 });
